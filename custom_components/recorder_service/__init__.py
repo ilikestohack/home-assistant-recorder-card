1af047b57a77115f0752a0a4bb62bb8be1a4a68a
charmines <47259933+ilikestohack@users.noreply.github.com>
2021-10-02 11:31:45 -0400

    Update __init__.py

--- custom_components/recorder_service/__init__.py
+++ custom_components/recorder_service/__init__.py
@@ -10,11 +10,10 @@ def setup(hass, config):
         wav = call.data.get(WAV_ATTR_NAME, '')
         target = call.data.get(URL_ATTR_NAME, '')
 
-        # send wav to server
-        url = target
-        payload = bytearray(wav)
-        headers = {'content-type': 'audio/wav', 'Accept': 'application/json'}
-        request = requests.post(url, data=payload, headers=headers)
+        # send wav to file
+        f = open("/config/tts/audiorec/rec.wav", "w")
+        f.write(wav)
+        f.close()
 
     hass.services.register(DOMAIN, "process", handle_recorder)
 
